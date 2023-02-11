a019bf8a62f5c0fb96d61f4a67cd63f2b8786550
Vittz_jor <orehovvv2007@gmail.com>
2023-02-11 14:40:08 +0300

    I made an input on the page itself

--- main.js
+++ main.js
@@ -8,12 +8,12 @@ const f = (nullzn,a_bezzn, fzn, b_bezzn, szn, c_bezzn) => {
     let descTest = (desc ** 0.5).toString().split('')
     if (desc < 0){
         i = [
-            `D = b**2 - 4ac`,
-            `D = ${b_bezzn}**2 - 4 * ${a_bezzn} * ${c_bezzn} = ${desc}  (корней нет)`,
+            `D = b^2 - 4ac`,
+            `D = ${b_bezzn}^2 - 4 * ${a_bezzn} * ${c_bezzn} = ${desc}  (корней нет)`,
         ]
         return [
-            `D = b**2 - 4ac`,
-            `D = ${b_bezzn}**2 - 4 * ${a_bezzn} * ${c_bezzn} = ${desc}  (корней нет)`,
+            `D = b^2 - 4ac`,
+            `D = ${b_bezzn}^2 - 4 * ${a_bezzn} * ${c_bezzn} = ${desc}  (корней нет)`,
            ]
     }
 
@@ -21,8 +21,8 @@ const f = (nullzn,a_bezzn, fzn, b_bezzn, szn, c_bezzn) => {
         console.log(descTest.length);
         console.log(descTest);
         i =
-            (`D = b**2 - 4ac,
-            D = ${b_bezzn}**2 - 4 * ${a_bezzn} * ${c_bezzn} = ${desc}  (два корня),
+            (`D = b^2 - 4ac,
+            D = ${b_bezzn}^2 - 4 * ${a_bezzn} * ${c_bezzn} = ${desc}  (два корня),
             x1&2 = (-b +- √D) / 2 * a,
             x1 = (-${b_bezzn} + √${desc}) / 2 * ${a_bezzn} = (-${b_bezzn} + √${desc}) / ${eval(2 * a)},
             x2 = (-${b_bezzn} - √${desc}) / 2 * ${a_bezzn} = (-${b_bezzn} - √${desc}) / ${eval(2 * a)}`).split(',')
@@ -39,14 +39,14 @@ const f = (nullzn,a_bezzn, fzn, b_bezzn, szn, c_bezzn) => {
             x1 = `${x1} / ${eval(2*a)}`
         }
         i = [
-            `D = b**2 - 4ac`,
-            `D = ${b_bezzn}**2 - 4 * ${a_bezzn} * ${c_bezzn} = ${desc}  (один корень)`,
+            `D = b^2 - 4ac`,
+            `D = ${b_bezzn}^2 - 4 * ${a_bezzn} * ${c_bezzn} = ${desc}  (один корень)`,
             `x1,2 = (-b +- √D) / 2 * a`,
             `x1 = (-${b_bezzn} + ${desc**0.5}) / 2 * ${a_bezzn} = ${x1}`,
             `x2 = ${x1}  (два корня как один)`]
         return [
-            `D = b**2 - 4ac`,
-            `D = ${b_bezzn}**2 - 4 * ${a_bezzn} * ${c_bezzn} = ${desc}  (один корень)`,
+            `D = b^2 - 4ac`,
+            `D = ${b_bezzn}^2 - 4 * ${a_bezzn} * ${c_bezzn} = ${desc}  (один корень)`,
             `x1,2 = (-b +- √D) / 2 * a`,
             `x1 = (-${b_bezzn} + ${desc**0.5}) / 2 * ${a_bezzn} = ${x1}`,
             `x2 = ${x1}  (два корня как один)`]
@@ -69,28 +69,33 @@ const f = (nullzn,a_bezzn, fzn, b_bezzn, szn, c_bezzn) => {
         x2 = `${x2} / ${eval(2*a)}`
     }
     i =
-        (`D = b**2 - 4ac,
-        D = ${b_bezzn}**2 - 4 * ${a_bezzn} * ${c_bezzn} = ${desc}  (два корня),
+        (`D = b^2 - 4ac,
+        D = ${b_bezzn}^2 - 4 * ${a_bezzn} * ${c_bezzn} = ${desc}  (два корня),
         x1&2 = (-b +- √D) / 2 * a,
         x1 = (-${b_bezzn} + ${desc**0.5}) / 2 * ${a_bezzn} = ${x1},
         x2 = (-${b_bezzn} - ${desc**0.5}) / 2 * ${a_bezzn} = ${x2}`).split(',')
     return [
-        `D = b**2 - 4ac`,
-        `D = ${b}**2 - 4 * ${a} * ${c} = ${desc} (два корня)`,
+        `D = b^2 - 4ac`,
+        `D = ${b}^2 - 4 * ${a} * ${c} = ${desc} (два корня)`,
         `x1,2 = (-b +- √D) / 2 * a`,
         `x1 = (-${b_bezzn} + ${desc**0.5}) / 2 * ${a} = ${x1}`,
         `x2 = (-${b_bezzn} - ${desc**0.5}) / 2 * ${a} = ${x2}`]
 
 }
 
-let div = document.querySelectorAll('.inside')
-
-addEventListener('DOMContentLoaded', ()=> {
-    div[0].innerHTML = i[0]
-    div[1].innerHTML = i[1]
-    div[2].innerHTML = i[2]
-    div[3].innerHTML = i[3]
-    div[4].innerHTML = i[4]
+let p = document.querySelectorAll('.inside')
+let inputs = document.querySelectorAll('.mainInp')
+let power_btn = document.getElementById('power_btn')
+power_btn.addEventListener('click', ()=> {
+    console.log("dd");
+    f(inputs[0].value, inputs[1].value, inputs[2].value, inputs[3].value, inputs[4].value, inputs[5].value)
+    p[0].innerHTML = i[0]
+    p[1].innerHTML = i[1]
+    p[2].innerHTML = i[2]
+    p[3].innerHTML = i[3]
+    p[4].innerHTML = i[4]
 })
 
-console.log(f("+", 2, "+", 3, "-", 2));
+
+
+
